60b27fb0f4b1f991d9d8f5dbb5b3637c1666abfa
unknown <pariharansh123@.com>
2022-11-04 18:27:26 +0530

    check the prime code

---
+++ prime.js
@@ -0,0 +1,17 @@
+function checkPrime(num)
+let factor=1;
+for(let i=1;i<=n;i++)
+{
+if(num%2==0)
+{
+return true
+}
+retuen false
+if(factor==1)
+{
+console.log("Prime Number:)
+}
+else{
+console.log("Not Prime Number")
+}
+checkPrime();
